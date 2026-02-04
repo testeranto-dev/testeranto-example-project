527a8687613c1de642c7cc0b9cc1c3cd179cd64c
adam wong <adamwong246@gmail.com>
2026-02-03 16:28:42 -0800

    node works

--- src/ts/Calculator.test.ts
+++ src/ts/Calculator.test.ts
@@ -8,15 +8,15 @@ console.log("hello Calculator", Calculator)
 import Tiposkripto from "tiposkripto";
 console.log("hello Tiposkripto", Tiposkripto)
 
-// import { adapter } from "./Calculator.test.adapter.js";
-// import { implementation } from "./Calculator.test.implementation.js";
-// import { specification } from "./Calculator.test.specification.js";
-// import { I, O, M } from "./Calculator.test.types.js";
+import { adapter } from "./Calculator.test.adapter.js";
+import { implementation } from "./Calculator.test.implementation.js";
+import { specification } from "./Calculator.test.specification.js";
+import { I, O, M } from "./Calculator.test.types.js";
 
-// export default Tiposkripto<I, O, M>(
-//   Calculator,
-//   specification,
-//   implementation,
-//   adapter,
-//   { ports: 1000 }
-// );
+export default Tiposkripto<I, O, M>(
+  Calculator,
+  specification,
+  implementation,
+  adapter,
+  { ports: 1000 }
+);
